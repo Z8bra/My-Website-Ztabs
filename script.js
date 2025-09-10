7ea7ee2a0de5904363e63afb013685ec95efdd3a
Z8bra <110273912+Z8bra@users.noreply.github.com>
2025-09-10 16:57:36 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function addTab() {
+  const input = document.getElementById('newTabInput');
+  const name = input.value.trim();
+  if(name === "") return alert("Enter a tab name!");
+  
+  const ul = document.getElementById('tabList');
+  const li = document.createElement('li');
+  li.textContent = name;
+  ul.appendChild(li);
+  
+  input.value = "";
+}
 
